dc96c8a990719ea39f20af20bc4d37ff92d59db2
Zoe Monet Williams <mwilliamszoe@gmail.com>
2019-03-14 12:01:07 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function getName(node){
+  return node.name
+}
+
+function headNode(linkedList, collection){
+  return collection[linkedList]
+}
+
+function next(node, collection){
+  let nextAddress = node.nextAddress
+  return collection(`${nextAddress}`)
+}
+
+function nodeAt(index, linkedList, collection){
+  let currentNode = headNode(linkedList, collection);
+  for(let i=0; i<index; i++){
+    currentNode = next(currentNode, collection);
+  }
+  return currentNode
+}
+
+function addressAt(index, linkedList, collection){
+  if(index == 0){
+    return linkedList
+  } else {
+    let node = nodeAt(index-1, linkedList, collection)
+    return node.next
+  }
+}
+
+function indexAt(node, collection, linkedList){
+  let currentNode = headNode(linkedList, collection);
+  let currentIdx = 0
+  while(currentNode != node){
+    current Idx++
+    currentNode = next(currentNode, collection)
+  }
+  return current Idx
+}
